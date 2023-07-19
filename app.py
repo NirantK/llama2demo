66c66839fe49c8e7f90cc7d9d7426338b9ae8caf
NirantK <nirant.bits@gmail.com>
2023-07-19 10:30:49 +0530

    * chore(app.py): add temperature parameter to the Replicate API request

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ if prompt := st.chat_input("What is up?"):
             input={
                 "prompt": prompt,
                 "max_tokens": 100,
+                "temperature": 0.1,
             },
         )
     # The a16z-infra/llama13b-v2-chat model can stream output as it's running.
