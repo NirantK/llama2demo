19efd715eb51682f25e7d883b799c2e95354ac0f
NirantK <nirant.bits@gmail.com>
2023-07-19 12:30:35 +0530

    * refactor(app.py): remove unnecessary line breaks and indentation * feat(app.py): add message history to the input prompt for better context

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ llama_family = {
     "Llama13B-v2-Chat": "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
     "Llama70B-v2-Chat": "replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48",
 }
-model_choice = st.selectbox(
-    "Model", options=tuple(llama_family.keys())
-)
+model_choice = st.selectbox("Model", options=tuple(llama_family.keys()))
 llm_model = llama_family[model_choice]
 st.session_state["llm_model"] = llm_model
 if "messages" not in st.session_state:
@@ -31,11 +29,16 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-
+        message_history = "\n".join(
+            [
+                "{message:['role']}: {message['content']}"
+                for message in st.session_state.messages
+            ]
+        )
         output = replicate.run(
             llm_model,
             input={
-                "prompt": f"User: {prompt}\nAssistant:",
+                "prompt": "f{message_history}\nUser: {prompt}\nAssistant: }",
                 "max_tokens": 100,
                 "temperature": 0.1,
                 "top_p": 0.1,
