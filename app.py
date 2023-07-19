e98583ef22a06c3f60ee6af650d89bde2e4f582e
NirantK <nirant.bits@gmail.com>
2023-07-19 14:37:09 +0530

    * chore(app.py): refactor sidebar code to use st.sidebar() and add markdown heading for Model Parameters

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ st.session_state["llm_model"] = llm_model
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-with st.sidebar.title("Model Settings"):
+with st.sidebar():
+    st.markdown("## Model Parameters")
     # repetition_penalty = st.slider("Repetition Penalty", 0.0, 2.0, 1.0, help="Penalty for repeated words in generated text; 1 is no penalty, values greater than 1 discourage repetition, less than 1 encourage it.")
     temperature = st.slider("Temperature", 0.01, 5.0, 0.9, help="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.")
     max_tokens = st.slider("Max Length in Number of Tokens", 10, 500, 100, help="Maximum number of tokens to generate. A word is generally 2-3 tokens")
